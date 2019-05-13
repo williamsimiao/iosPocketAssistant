66e5140366872fc30e92615211e1d7e4a484dc57
William Simiao <williamsimiao@gmail.com>
2019-05-13 14:13:55 -0300

    Initial Commit

---
+++ pocketAssistant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pocketAssistant
+//
+//  Created by William Simiao on 13/05/19.
+//  Copyright © 2019 William Simiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pocketAssistantTests/pocketAssistantTests.swift
@@ -0,0 +1,34 @@
+//
+//  pocketAssistantTests.swift
+//  pocketAssistantTests
+//
+//  Created by William Simiao on 13/05/19.
+//  Copyright © 2019 William Simiao. All rights reserved.
+//
+
+import XCTest
+@testable import pocketAssistant
+
+class pocketAssistantTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
