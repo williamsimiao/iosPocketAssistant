c1e3e8e36d65aa986b160cbe57bd47d73d2f70ba
William Simiao <williamsimiao@gmail.com>
2019-05-28 17:31:25 -0300

    ViewController on interface builder

--- pocketAssistant/ViewController/ViewController.swift
+++ pocketAssistant/ViewController/ViewController.swift
@@ -13,204 +13,43 @@ import SwiftKeychainWrapper
 class MainViewController: UIViewController {
     
     let networkManager = NetworkManager()
-//    var tokenString: String?
     
-    let scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.translatesAutoresizingMaskIntoConstraints = false;
-        scrollView.backgroundColor = .white
-        scrollView.layoutMargins = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
-        return scrollView
-    }()
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var usernameTextField: MDCTextField!
+    @IBOutlet weak var passwordTextField: MDCTextField!
+    @IBOutlet weak var otpTextField: MDCTextField!
     
     var appBarViewController = MDCAppBarViewController()
-
-    // Text Fields
-    //TODO: Add text fields
-    let usernameTextField: MDCTextField = {
-        let usernameTextField = MDCTextField()
-        usernameTextField.text = "master"
-        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
-        usernameTextField.clearButtonMode = .unlessEditing
-        usernameTextField.autocapitalizationType = UITextAutocapitalizationType.none
-        return usernameTextField
-    }()
-    let passwordTextField: MDCTextField = {
-        let passwordTextField = MDCTextField()
-        passwordTextField.text = "12345678"
-        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
-        passwordTextField.isSecureTextEntry = true
-        return passwordTextField
-    }()
-    let otpTextField: MDCTextField = {
-        let otpTextField = MDCTextField()
-        otpTextField.translatesAutoresizingMaskIntoConstraints = false
-        return otpTextField
-    }()
-    
     
     //TODO: Add text field controllers
-    let usernameTextFieldController: MDCTextInputControllerOutlined
-    let passwordTextFieldController: MDCTextInputControllerOutlined
-    let otpTextFieldController: MDCTextInputControllerOutlined
-
-    
-    // Buttons
-    let autenticarButton: MDCButton = {
-        let autenticarButton = MDCButton()
-        autenticarButton.translatesAutoresizingMaskIntoConstraints = false
-        autenticarButton.setTitle("AUTENTICAR", for: .normal)
-        autenticarButton.addTarget(self, action: #selector(didTapNext(sender:)), for: .touchUpInside)
-        return autenticarButton
-    }()
-    
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        //TODO: Setup text field controllers
-        usernameTextFieldController = MDCTextInputControllerOutlined(textInput: usernameTextField)
-        passwordTextFieldController = MDCTextInputControllerOutlined(textInput: passwordTextField)
-        otpTextFieldController = MDCTextInputControllerOutlined(textInput: otpTextField)
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        usernameTextFieldController = MDCTextInputControllerOutlined(textInput: usernameTextField)
-        passwordTextFieldController = MDCTextInputControllerOutlined(textInput: passwordTextField)
-        otpTextFieldController = MDCTextInputControllerOutlined(textInput: otpTextField)
-        super.init(coder: aDecoder)
-    }
+    var usernameTextFieldController: MDCTextInputControllerOutlined?
+    var passwordTextFieldController: MDCTextInputControllerOutlined?
+    var otpTextFieldController: MDCTextInputControllerOutlined?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Dinâmo Pocket"
+        usernameTextFieldController = MDCTextInputControllerOutlined(textInput: usernameTextField)
+        passwordTextFieldController = MDCTextInputControllerOutlined(textInput: passwordTextField)
+        otpTextFieldController = MDCTextInputControllerOutlined(textInput: otpTextField)
 
+        self.title = "Dinâmo Pocket"
         // Set the tracking scroll view.
-        self.appBarViewController.headerView.trackingScrollView = self.scrollView
-        scrollView.backgroundColor = .white
         scrollView.delegate = self
         
-        view.addSubview(scrollView)
-        
-        NSLayoutConstraint.activate(
-            NSLayoutConstraint.constraints(withVisualFormat: "V:|[scrollView]|",
-                                           options: [],
-                                           metrics: nil,
-                                           views: ["scrollView" : scrollView])
-        )
-        NSLayoutConstraint.activate(
-            NSLayoutConstraint.constraints(withVisualFormat: "H:|[scrollView]|",
-                                           options: [],
-                                           metrics: nil,
-                                           views: ["scrollView" : scrollView])
-        )
+        self.appBarViewController.headerView.trackingScrollView = self.scrollView
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapTouch))
         scrollView.addGestureRecognizer(tapGestureRecognizer)
         
         self.addChild(self.appBarViewController)
         self.appBarViewController.didMove(toParent: self)
         view.addSubview(self.appBarViewController.view)
-        
-        // TextFields
-        //TODO: Add text fields to scroll view and setup initial state
-        scrollView.addSubview(usernameTextField)
-        scrollView.addSubview(passwordTextField)
-        scrollView.addSubview(otpTextField)
-        usernameTextFieldController.placeholderText = "Usuario"
+
         usernameTextField.delegate = self
-        passwordTextFieldController.placeholderText = "Senha"
         passwordTextField.delegate = self
-        otpTextFieldController.placeholderText = "OTP(Opcional)"
         otpTextField.delegate = self
         registerKeyboardNotifications()
         
-        // Buttons
-        //TODO: Add buttons to the scroll view
-        scrollView.addSubview(autenticarButton)
         
-        // Constraints
-        var constraints = [NSLayoutConstraint]()
-        
-        // Text Fields
-        constraints.append(NSLayoutConstraint(item: usernameTextField,
-                                              attribute: .top,
-                                              relatedBy: .equal,
-                                              toItem: scrollView,
-                                              attribute: .bottom,
-                                              multiplier: 1,
-                                              constant: 150))
-        constraints.append(NSLayoutConstraint(item: usernameTextField,
-                                              attribute: .centerX,
-                                              relatedBy: .equal,
-                                              toItem: scrollView,
-                                              attribute: .centerX,
-                                              multiplier: 1,
-                                              constant: 0))
-        constraints.append(contentsOf:
-            NSLayoutConstraint.constraints(withVisualFormat: "H:|-[username]-|",
-                                           options: [],
-                                           metrics: nil,
-                                           views: [ "username" : usernameTextField]))
-        constraints.append(NSLayoutConstraint(item: passwordTextField,
-                                              attribute: .top,
-                                              relatedBy: .equal,
-                                              toItem: usernameTextField,
-                                              attribute: .bottom,
-                                              multiplier: 1,
-                                              constant: 8))
-        constraints.append(NSLayoutConstraint(item: passwordTextField,
-                                              attribute: .centerX,
-                                              relatedBy: .equal,
-                                              toItem: scrollView,
-                                              attribute: .centerX,
-                                              multiplier: 1,
-                                              constant: 0))
-        constraints.append(contentsOf:
-            NSLayoutConstraint.constraints(withVisualFormat: "H:|-[password]-|",
-                                           options: [],
-                                           metrics: nil,
-                                           views: [ "password" : passwordTextField]))
-        //
-        constraints.append(NSLayoutConstraint(item: otpTextField,
-                                              attribute: .top,
-                                              relatedBy: .equal,
-                                              toItem: passwordTextField,
-                                              attribute: .bottom,
-                                              multiplier: 1,
-                                              constant: 8))
-        constraints.append(NSLayoutConstraint(item: otpTextField,
-                                              attribute: .centerX,
-                                              relatedBy: .equal,
-                                              toItem: scrollView,
-                                              attribute: .centerX,
-                                              multiplier: 1,
-                                              constant: 0))
-        constraints.append(contentsOf:
-            NSLayoutConstraint.constraints(withVisualFormat: "H:|-[otp]-|",
-                                           options: [],
-                                           metrics: nil,
-                                           views: [ "otp" : otpTextField]))
-        
-        // Buttons
-        constraints.append(NSLayoutConstraint(item: autenticarButton,
-                                              attribute: .top,
-                                              relatedBy: .equal,
-                                              toItem: otpTextField,
-                                              attribute: .bottom,
-                                              multiplier: 1,
-                                              constant: 8))
-        constraints.append(NSLayoutConstraint(item: autenticarButton,
-                                              attribute: .centerX,
-                                              relatedBy: .equal,
-                                              toItem: scrollView,
-                                              attribute: .centerX,
-                                              multiplier: 1,
-                                              constant: 0))
-        constraints.append(contentsOf:
-            NSLayoutConstraint.constraints(withVisualFormat: "H:|-[autenticar]-|",
-                                           options: [],
-                                           metrics: nil,
-                                           views: [ "autenticar" : autenticarButton]))
-        
-        NSLayoutConstraint.activate(constraints)
     }
     
     // MARK: - Gesture Handling
@@ -240,7 +79,7 @@ class MainViewController: UIViewController {
         return true
     }
     
-    @objc func didTapNext(sender: Any) {
+    @IBAction func didTapAutenticar(_ sender: Any) {
         if validateAuth() == false {
             return
         }
@@ -257,14 +96,13 @@ class MainViewController: UIViewController {
                 if !(tokenSaved && usrSaved && pwdSaved) {
                     return
                 }
-
+                
                 DispatchQueue.main.async {
                     self.performSegue(withIdentifier: "to_second", sender: self)
                 }
             }
         }
     }
-    
     
     // MARK: - Keyboard Handling
     
@@ -295,27 +133,6 @@ class MainViewController: UIViewController {
     @objc func keyboardWillHide(notification: NSNotification) {
         self.scrollView.contentInset = UIEdgeInsets.zero;
     }
-    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        guard let identifier = segue.identifier else {
-//            return
-//        }
-//        if identifier == "to_second" {
-//            guard let navigationController = segue.destination as? UINavigationController else {
-//                return
-//            }
-//
-//            guard let secondViewController = navigationController.viewControllers.first as? SecondViewController else {
-//                return
-//            }
-//            guard let token = self.tokenString else {
-//                print("NO token")
-//                return
-//            }
-//            secondViewController.tokenString = token
-//            secondViewController.networkManager = self.networkManager
-//        }
-//    }
 }
 
 
@@ -330,7 +147,7 @@ extension MainViewController: UITextFieldDelegate {
         if (textField == passwordTextField &&
             passwordTextField.text != nil &&
             passwordTextField.text!.count < 8) {
-            passwordTextFieldController.setErrorText("Senha muito curta",
+            passwordTextFieldController!.setErrorText("Senha muito curta",
                                                      errorAccessibilityValue: nil)
         }
         
