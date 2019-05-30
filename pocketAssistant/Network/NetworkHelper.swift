bf4452056b76af2adfeb1fce5748bf0883b3f362
William Simiao <williamsimiao@gmail.com>
2019-05-30 11:59:15 -0300

    added isConnectedToNetwork

---
+++ pocketAssistant/Network/NetworkHelper.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkHelper.swift
+//  pocketAssistant
+//
+//  Created by William Simiao on 30/05/19.
+//  Copyright © 2019 William Simiao. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+
+func isConnectedToNetwork() -> Bool {
+    var zeroAddress = sockaddr_in()
+    
+    zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+    zeroAddress.sin_family = sa_family_t(AF_INET)
+    
+    let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+        $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+            SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+        }
+    }
+    
+    var flags = SCNetworkReachabilityFlags()
+    if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+        return false
+    }
+    
+    let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+    let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+    
+    return (isReachable && !needsConnection)
+}
