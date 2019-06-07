82c1eae5740dbd0b06e7b257b5285639df168188
William <williamsimiao@gmail.com>
2019-06-06 23:49:33 -0300

    mudanca para usar a lista de TRUSTER e TRUSTEES ao invez do nome

--- pocketAssistant/ViewController/NovaPermissaoViewController.swift
+++ pocketAssistant/ViewController/NovaPermissaoViewController.swift
@@ -12,7 +12,6 @@ import SwiftKeychainWrapper
 
 class NovaPermissaoViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var usernameTextField: MDCTextField!
     @IBOutlet weak var lerSwitch: UISwitch!
     @IBOutlet weak var criarSwitch: UISwitch!
     @IBOutlet weak var removerSwitch: UISwitch!
@@ -22,14 +21,49 @@ class NovaPermissaoViewController: UIViewController {
     var usernameTextFieldController: MDCTextInputControllerOutlined?
     let networkManager = NetworkManager()
     var myAcl: aclStruct?
-    var userName: String?
+    var username: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        usernameTextFieldController = MDCTextInputControllerOutlined(textInput: usernameTextField)
-        usernameTextField.delegate = self
-        registerKeyboardNotifications()
+        salvarButton.applyContainedTheme(withScheme: globalContainerScheme())
+        lerSwitch.addTarget(self, action: #selector(switchChanged), for: UIControl.Event.valueChanged)
+        criarSwitch.addTarget(self, action: #selector(switchChanged), for: UIControl.Event.valueChanged)
+        removerSwitch.addTarget(self, action: #selector(switchChanged), for: UIControl.Event.valueChanged)
+        atualizarSwitch.addTarget(self, action: #selector(switchChanged), for: UIControl.Event.valueChanged)
+
+        getAclRequest()
+    }
+    
+    // MARK: - SWITCHES
+    @objc func switchChanged(mySwitch: UISwitch) {
+        switch mySwitch {
+        case lerSwitch:
+            if mySwitch.isOn == false {
+                criarSwitch.isOn = false
+                removerSwitch.isOn = false
+                atualizarSwitch.isOn = false
+            }
+
+        default:
+            print("Outro switch")
+        }
+    }
+    
+    func getIntFromSwitches() -> Int {
+        var unionOfBits = aclStruct(rawValue: 0)
+        if lerSwitch.isOn {
+            unionOfBits = unionOfBits.union(.obj_read)
+        }
+        if criarSwitch.isOn {
+            unionOfBits = unionOfBits.union(.obj_create)
+        }
+        if removerSwitch.isOn {
+            unionOfBits = unionOfBits.union(.obj_del)
+        }
+        if atualizarSwitch.isOn {
+            unionOfBits = unionOfBits.union(.obj_update)
+        }
+        return Int(unionOfBits.rawValue)
     }
     
     func setUpSwitches(aclInteger: Int) {
@@ -52,18 +86,14 @@ class NovaPermissaoViewController: UIViewController {
         if myAcl.contains(.obj_update) {
             atualizarSwitch.isOn = true
         }
-        
     }
     
-    @IBAction func didTapSalvar(_ sender: Any) {
-        
-    }
-    
+    // MARK: - GET ACL
     func getAclRequest() {
         guard let token = KeychainWrapper.standard.string(forKey: "TOKEN") else {
             return
         }
-        guard let username = usernameTextField.text else {
+        guard let username = self.username else {
             return
         }
         
@@ -81,15 +111,18 @@ class NovaPermissaoViewController: UIViewController {
     }
     
     
-    func updateAclRequest() {
+    // MARK: - UPDATE ACL
+    @IBAction func didTapSalvar(_ sender: Any) {
+        
+    }
+    
+    func updateAclRequest(acl: Int) {
         guard let token = KeychainWrapper.standard.string(forKey: "TOKEN") else {
             return
         }
-        guard let username = usernameTextField.text else {
+        guard let username = username else {
             return
         }
-        //TODO: Mudar, montando a partir dos valores dos switches
-        let acl = 15
         networkManager.runUpdateAcl(token: token, acl: acl, usr: username) { (error) in
             if let error = error {
                 print(error)
@@ -105,59 +138,59 @@ class NovaPermissaoViewController: UIViewController {
         }
     }
     
-    // MARK: - Keyboard Handling
-    func registerKeyboardNotifications() {
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(self.keyboardWillShow),
-            name: NSNotification.Name(rawValue: "UIKeyboardWillShowNotification"),
-            object: nil)
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(self.keyboardWillShow),
-            name: NSNotification.Name(rawValue: "UIKeyboardWillChangeFrameNotification"),
-            object: nil)
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(self.keyboardWillHide),
-            name: NSNotification.Name(rawValue: "UIKeyboardWillHideNotification"),
-            object: nil)
-    }
-
-    @objc func keyboardWillShow(notification: NSNotification) {
-        let keyboardFrame =
-            (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
-        self.scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardFrame.height, right: 0);
-    }
-
-    @objc func keyboardWillHide(notification: NSNotification) {
-        self.scrollView.contentInset = UIEdgeInsets.zero;
-    }
+//    // MARK: - Keyboard Handling
+//    func registerKeyboardNotifications() {
+//        NotificationCenter.default.addObserver(
+//            self,
+//            selector: #selector(self.keyboardWillShow),
+//            name: NSNotification.Name(rawValue: "UIKeyboardWillShowNotification"),
+//            object: nil)
+//        NotificationCenter.default.addObserver(
+//            self,
+//            selector: #selector(self.keyboardWillShow),
+//            name: NSNotification.Name(rawValue: "UIKeyboardWillChangeFrameNotification"),
+//            object: nil)
+//        NotificationCenter.default.addObserver(
+//            self,
+//            selector: #selector(self.keyboardWillHide),
+//            name: NSNotification.Name(rawValue: "UIKeyboardWillHideNotification"),
+//            object: nil)
+//    }
+//
+//    @objc func keyboardWillShow(notification: NSNotification) {
+//        let keyboardFrame =
+//            (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+//        self.scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardFrame.height, right: 0);
+//    }
+//
+//    @objc func keyboardWillHide(notification: NSNotification) {
+//        self.scrollView.contentInset = UIEdgeInsets.zero;
+//    }
 }
 
-// MARK: - UITextFieldDelegate
-extension NovaPermissaoViewController: UITextFieldDelegate {
-
-    func textFieldShouldClear(_ textField: UITextField) -> Bool {
-        usernameTextFieldController?.setErrorText(nil, errorAccessibilityValue: nil)
-        return true
-    }
-
-    //Validation after press return
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        if usernameTextField.text == nil || usernameTextField.text == "" {
-            usernameTextFieldController?.setErrorText("Campo obrigatório", errorAccessibilityValue: nil)
-        }
+//// MARK: - UITextFieldDelegate
+//extension NovaPermissaoViewController: UITextFieldDelegate {
+//
+//    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+//        usernameTextFieldController?.setErrorText(nil, errorAccessibilityValue: nil)
 //        return true
-        return false
-    }
-
-    //Validation while typing
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-
-
-        return true
-    }
-}
+//    }
+//
+//    //Validation after press return
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        textField.resignFirstResponder()
+//        if usernameTextField.text == nil || usernameTextField.text == "" {
+//            usernameTextFieldController?.setErrorText("Campo obrigatório", errorAccessibilityValue: nil)
+//        }
+////        return true
+//        return false
+//    }
+//
+//    //Validation while typing
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//
+//
+//        return true
+//    }
+//}
 
